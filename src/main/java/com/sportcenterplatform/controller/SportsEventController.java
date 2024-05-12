0a54a1252177b91882ad59cb8ee8a66ba3b3ff44
Anhelina Kulkova <angeltutova@gmail.com>
2024-05-12 14:30:51 +0200

    Added the control of user role to create new event feature

--- src/main/java/com/sportcenterplatform/controller/SportsEventController.java
+++ src/main/java/com/sportcenterplatform/controller/SportsEventController.java
@@ -3,6 +3,8 @@ package com.sportcenterplatform.controller;
 
 import com.sportcenterplatform.service.SportsEventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +23,11 @@ public class SportsEventController {
     public String showAll(Model model) {
         model.addAttribute("events", sportsEventService.getAllSportsEvents());
         return "home";
+    }
+    @GetMapping("/new")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String showNewForm(Model model){
+        return "newEventForm";
     }
 
 }
