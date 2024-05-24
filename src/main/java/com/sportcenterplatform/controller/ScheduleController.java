a2973087ae58ca8333540de3c5c923e7b5175d4e
Anhelina Kulkova <angeltutova@gmail.com>
2024-05-24 18:15:34 +0200

    Implemented the get method for showing the New Schedule form

--- src/main/java/com/sportcenterplatform/controller/ScheduleController.java
+++ src/main/java/com/sportcenterplatform/controller/ScheduleController.java
@@ -1,13 +1,19 @@
 package com.sportcenterplatform.controller;
 
+import com.sportcenterplatform.dto.ScheduleNewDTO;
+import com.sportcenterplatform.dto.SportEventNewDTO;
+import com.sportcenterplatform.entity.SportType;
 import com.sportcenterplatform.service.ScheduleService;
 import com.sportcenterplatform.service.SportsEventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/schedules")
@@ -27,5 +33,13 @@ public class ScheduleController {
         model.addAttribute("schedules", scheduleService.getAllBySportsEventId(
                 sportsEventService.getSportsEventEntityById(id)));
         return "schedules";
+    }
+    @GetMapping("/new")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String showNewForm(@RequestParam(value = "eventId") Long eventId, Model model){
+        model.addAttribute("event", sportsEventService.getSportsEventById(eventId));
+        model.addAttribute("schedule", new ScheduleNewDTO(LocalDateTime.now(), LocalDateTime.now(), 10, 0L));
+
+        return "newScheduleForm";
     }
 }
---
+++ src/main/java/com/sportcenterplatform/dto/ScheduleNewDTO.java
@@ -0,0 +1,11 @@
+package com.sportcenterplatform.dto;
+
+import java.time.LocalDateTime;
+
+public record ScheduleNewDTO(
+        LocalDateTime startTime,
+        LocalDateTime endTime,
+        Integer capacity,
+        Long sportsEventId
+) {
+}
