f15d614ef8ec9ba033fa67f7f9709d4646e417ba
Anhelina Kulkova <angeltutova@gmail.com>
2024-05-24 19:34:08 +0200

    Prepared the endpoint in controller for post /schedule

--- src/main/java/com/sportcenterplatform/controller/ScheduleController.java
+++ src/main/java/com/sportcenterplatform/controller/ScheduleController.java
@@ -9,9 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -41,5 +39,12 @@ public class ScheduleController {
         model.addAttribute("schedule", new ScheduleNewDTO(LocalDateTime.now(), LocalDateTime.now(), 10, 0L));
 
         return "newScheduleForm";
+    }
+    @PostMapping()
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String save(@RequestParam(value = "eventId") Long eventId,
+                       @ModelAttribute ScheduleNewDTO schedule){
+        System.out.println(eventId);
+        return "redirect:/schedules?id="+ eventId;
     }
 }
